6f893e176bb388d8b412181aac3fd56029473fec
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-08-10 21:28:18 -0500

    test fix 2

--- includes/shortcodes/search.php
+++ includes/shortcodes/search.php
@@ -98,7 +98,7 @@ function serlib_buscador_home_results_shortcode(){
         $imagen =  wp_get_attachment_image_src( get_post_thumbnail_id( $categorias_publicacion[$i]->post_id ), 'medium' )[0];
         $iconos[$titulo[1]] = [ $url, $icono, $imagen];
         
-        var_dump(get_post_thumbnail_id($categorias_publicacion[$i]->post_id));
+        var_dump(get_the_post_thumbnail_url($categorias_publicacion[$i]->post_id));
     }
 
     $formHTML = '
