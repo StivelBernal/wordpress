aac0f97a5ba57431b9b7a60fad91cef79f2c501e
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-08-18 12:48:54 -0500

    categorias en lsita de publicaciones turista

--- assets/html/frontend/all-post.php
+++ assets/html/frontend/all-post.php
@@ -16,7 +16,7 @@ echo wp_nonce_field( 'serlib_form', '_wpnonce', true, false );
     <div class="crud-wrapper">
         <div class="item-wrapper">
             <strong class="s-flex uppercase"><?php echo __('Titulo', 'serlib'); ?></strong>
-            <strong class="s-flex uppercase"><?php echo __('Extracto', 'serlib'); ?></strong>
+            <strong class="s-flex uppercase"><?php echo __('Categorias', 'serlib'); ?></strong>
             <strong class="s-flex uppercase text-center"><?php echo __('Imagen', 'serlib'); ?></strong>
             <strong class="s-10 uppercase text-center"><?php echo __('Acciones', 'serlib'); ?></strong>
            
@@ -28,7 +28,8 @@ echo wp_nonce_field( 'serlib_form', '_wpnonce', true, false );
                 <strong class="date">{{object.post_date }}</strong>
             </div>
             
-            <strong class="s-flex uppercase">{{object.post_excerpt}}</strong>
+            <div class="s-flex uppercase" style="font-weight:600;" ng-if="object.post_category !== false"><span ng-repeat="cat in object.post_category" ng-class="{in:!$first}" >{{cat.name}}</span></div>
+            <div class="s-flex uppercase" ng-if="object.post_category == false"><strong><?php echo __('sin categoria', 'serlib'); ?></strong> </div>
             <div class="s-flex text-center"> 
                 <img ng-src="{{object.thumbnail ? object.thumbnail: '/wp-content/plugins/ser_lib/assets/img/images.png'}}" width="80px">
             </div>
