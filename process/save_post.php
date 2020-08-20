df6870682f2083654842d5fce06f39e1e8d68165
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-08-19 22:34:52 -0500

    update

--- process/save_post.php
+++ process/save_post.php
@@ -4,7 +4,7 @@ function ser_save_post_admin( $post_id, $post, $update ){
     
     $data   =   get_post_meta( $post_id, 'activa', true );
 
-    if($data === 'RECHAZADO'){
+    if($data === 'RECHAZADO' && $post->post_status === 'pending'){
         $causa   =   get_post_meta( $post_id, 'causa_rechazo', true );
         if($causa === 'Otro'){
             $causa = get_post_meta( $post_id, 'causa_otro', true );
@@ -17,8 +17,17 @@ function ser_save_post_admin( $post_id, $post, $update ){
 
         enviar_email_rechazo($post_id, $causa);
        
+    }else if($data === 'ACTIVO' && $post->post_status === 'pending'){
+        
+        enviar_email_confirm($post_id);
+
     }
+
     
+}
+
+function tipo_de_contenido_html() {
+    return 'text/html';
 }
 
 function enviar_email_rechazo($post_id, $causa){
@@ -41,9 +50,9 @@ function enviar_email_rechazo($post_id, $causa){
                             </a>
                         </div>
                         <div style="margin: auto; display: block; text-align: left;">
-                            <p style="text-align: center; color: #5e5e5e; font-family: Poppins; font-size: x-large;">Tu publicación '.$post_c->post_author.' en el Golfo de Morrosquillo ha sido rechazada por el equipo de Golfo de Morrosquillo</p>
+                            <p style="text-align: center; color: #5e5e5e; font-family: Poppins; font-size: x-large;">Tu publicación '.$post_c->post_title.' en el Golfo de Morrosquillo ha sido rechazada a causa de '.$causa.' por el equipo de Golfo de Morrosquillo</p>
                             <p style="font-weight: 600; font-size:17px;">
-                                Accede a tu cuenta y crea una publicación que cumpla con los requisitos establecidos para nuestra Comunidad del Golfo de Morrosquillo
+                                Accede a tu cuenta y crea una publicación que cumpla con los requisitos establecidos para nuestra Comunidad del Golfo de Morrosquillo.
                             </p>
                             <a target="blank" href="https://golfodemorrosquillo.com/auth/">
                                 https://golfodemorrosquillo.com/auth/
@@ -53,24 +62,68 @@ function enviar_email_rechazo($post_id, $causa){
                             <br><br><p>Cordialmente,</p>
                             <img src="https://golfodemorrosquillo.com/wp-content/uploads/2020/08/a131e581-9844-44ea-bc79-d6385dbccee2.jpeg" width="120px">
                         </div>
-                        <div style="margin: auto; display: block; text-align: left;">
-                            '.$email.$causa.'    
+                    
+                    </body>
+                </html> '; 
+    
+      
+       add_filter( 'wp_mail_content_type', 'tipo_de_contenido_html' );
+   
+    $email = 'brayan.bernalg@gmail.com';
+
+    $mail_res = wp_mail( $email, '[Golfo de Morrosquillo] '._x('Publicación rechazada', 'asunto email', 'serlib') , $message, $headers );
+
+    return $mail_res;
+}
+
+
+function enviar_email_confirm($post_id){
+
+    $headers[]= 'From: Contacto <contacto@golfomorrosquillo.com>';
+
+    $post_c = get_post( $post_id );
+
+    $author = get_userdata($post_c->post_author);
+
+    $email = $author->user_email;
+
+    $message = '<html>
+                    <head>	
+                    </head>
+                    <body>
+                        <div style="margin: auto; display: block; flex-direction: column; text-align: center;">
+                            <a class="logo" href="https://golfodemorrosquillo.com" target="blank">
+                            <img src="https://golfodemorrosquillo.com/wp-content/uploads/2020/05/GDFRecurso-1MICOSCOLOR-e1588719554428.png" class="logo_main" width="300" >
+                            </a>
                         </div>
+                        <div style="margin: auto; display: block; text-align: left;">
+                            <p style="text-align: center; color: #5e5e5e; font-family: Poppins; font-size: x-large;">Tu publicación '.$post_c->post_title.' en el Golfo de Morrosquillo ha sido aprobada por el equipo del golfo de Morrosquillo</p>
+                            <p style="font-weight: 600; font-size:17px;">
+                                Accede a tu cuenta en el siguiente enlace:.
+                            </p>
+                            <a target="blank" href="https://golfodemorrosquillo.com/auth/">
+                                https://golfodemorrosquillo.com/auth/
+                            </a>
+                        </div>
+                        <div style="text-align: left;">
+                            <br><br><p>Cordialmente,</p>
+                            <img src="https://golfodemorrosquillo.com/wp-content/uploads/2020/08/a131e581-9844-44ea-bc79-d6385dbccee2.jpeg" width="120px">
+                        </div>
+                    
                     </body>
                 </html> '; 
             
     /**
     *Funcion para enviar el mensaje
     */ 
-    function tipo_de_contenido_html() {
-         return 'text/html';
-      }
-      
-       add_filter( 'wp_mail_content_type', 'tipo_de_contenido_html' );
    
-     $email = 'brayan.bernalg@gmail.com';
+      
+    add_filter( 'wp_mail_content_type', 'tipo_de_contenido_html' );
+   
+    $email = 'brayan.bernalg@gmail.com';
 
-    $mail_res = wp_mail( $email, '[Golfo de Morrosquillo] '._x('Confirmación de cuenta', 'asunto email', 'serlib') , $message, $headers );
+    $mail_res = wp_mail( $email, '[Golfo de Morrosquillo] Publicación aprobada', $message, $headers );
 
     return $mail_res;
+
 }
