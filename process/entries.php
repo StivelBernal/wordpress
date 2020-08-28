312dbec9877b8cd5ad8dfecd1722a53e158e6eb1
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-08-28 17:25:38 -0500

    sdf

--- process/entries.php
+++ process/entries.php
@@ -164,8 +164,8 @@ function serlib_entries_array($rol){
                     $user_meta=get_userdata($results[$i]->post_author);
                     $user_roles=$user_meta->roles[0];
                     
-                    if( $user_roles === 'aliado' ){
-                        echo 'dsfsdf';
+                    if( $user_roles == 'aliado' ){
+                        
                         $author = get_userdata($results[$i]->post_author);
                         $results[$i]->author = $author->user_login;
                         $thumb = get_the_post_thumbnail_url($results[$i]->ID);
