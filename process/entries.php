fc9b61a2b958fa00897ecc675d21bfdcb44ce265
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-08-28 16:56:47 -0500

    sddfs

--- process/entries.php
+++ process/entries.php
@@ -163,7 +163,7 @@ function serlib_entries_array($rol){
                 }
 
                 for($i = 0; $i < count($results); $i++){
-                    var_dump(wp_get_object_terms( $results[$i]->ID, 'category' ), 'sdfdsf', $categoria);
+                    var_dump(wp_get_object_terms( $results[$i]->ID, 'category', [ 'object_ids' => intval($categoria) ] ), [ 'object_ids' => intval($categoria) ]);
                     if( !empty(wp_get_post_categories($results[$i]->ID, [ 'object_ids' => intval($categoria) ] ) ) ){
                         $author = get_userdata($results[$i]->post_author);
                         $results[$i]->author = $author->user_login;
