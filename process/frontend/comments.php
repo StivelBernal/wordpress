96feeeee7efccf4e301c13313b83ff0bd2ff85f2
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-08-28 14:39:32 -0500

    sad

--- process/frontend/comments.php
+++ process/frontend/comments.php
@@ -20,8 +20,10 @@ function serlib_comments(){
             if(!empty($objDatos->stars)){
             update_comment_meta( $result->comment_ID, 'stars_items', $objDatos->stars );
             }
-            
+           
+
             if($result->comment_parent == 0){
+              
                 enviar_email_notificaciones_author_post($objDatos->post_id);
             }else{
 
