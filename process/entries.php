a7bde7de31a984906a6f001eb4d6bf879c70dafb
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-08-28 17:23:01 -0500

    sdf

--- process/entries.php
+++ process/entries.php
@@ -161,7 +161,7 @@ function serlib_entries_array($rol){
                 
                 for($i = 0; $i < count($results); $i++){
 
-                    $user_meta=get_userdata($results[0]->post_author);
+                    $user_meta=get_userdata($results[$i]->post_author);
                     $user_roles=$user_meta->roles[0];
                     echo $user_roles;
                     if( $user_roles === $rol ){
