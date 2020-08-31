79ffc211ac0ef7a0d3ae537e3fb90646eccfcb01
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-08-31 13:19:54 -0500

    sd

--- process/entries.php
+++ process/entries.php
@@ -167,7 +167,7 @@ function serlib_entries_array($rol){
                     return;
                 }
 
-                echo $categoria;
+                var_dump( $categoria );
                 $results = get_posts(["category" => $categoria ]);
                 
                 for($i = 0; $i < count($results); $i++){
